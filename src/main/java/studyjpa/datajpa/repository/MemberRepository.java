164e9d78b67267ab2c91ad6af347deb5d533a55f
02ggang9 <02ggang9@gmail.com>
2023-08-03 19:40:03 +0900

    Chapter4: @Query, DTO로 조회하기

--- src/main/java/studyjpa/datajpa/repository/MemberRepository.java
+++ src/main/java/studyjpa/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package studyjpa.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import studyjpa.datajpa.dto.MemberDto;
 import studyjpa.datajpa.entity.Member;
 
 import java.util.List;
@@ -11,6 +12,12 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new studyjpa.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 
 
 }
